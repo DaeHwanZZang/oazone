cfe4ea8f0482cb19ef891d336a8a27842cec9469
DaeHwanZZang <kimdh2670@gmail.com>
2026-08-08 16:28:52 +0900

    commit

---
+++ desktop/상품관련/상품상세페이지/shopdetail_original.js
@@ -0,0 +1,24 @@
+(function ($) {
+    // 사이즈 차트 관련 js
+    if ($('a.btn-size-chart')) {
+        $('a.btn-size-chart').click(function () {
+            var _chart = $('#sizeChart');
+            if (_chart) {
+                if (_chart.css('display') == 'block') {
+                    _chart.hide();
+                } else {
+                    var _parent = $(this).parent('td');
+                    var add_po = { top : 40, left: -352 };
+                    var _css = { 'top': (_parent.offset().top + add_po.top) + 'px', 'left': (_parent.offset().left + add_po.left) + 'px' };
+                    _chart.css(_css).show();
+                }
+                return false;
+            }
+        })
+    }
+    if ($('#sizeChart a.btn-close-layer')) {
+        $('#sizeChart a.btn-close-layer').click(function () {
+            $('#sizeChart').hide();
+        });
+    }
+})(jQuery);
